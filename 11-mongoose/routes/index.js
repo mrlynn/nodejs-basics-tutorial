9ccf8e6cc1df453128f3aea8c8958865a8c0e71e
Michael Lynn <merlynn@gmail.com>
2018-01-09 11:16:32 -0500

    Add stying

--- 11-mongoose/routes/index.js
+++ 11-mongoose/routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 mongoose.connect('localhost:27017/test');
+
+
 var Schema = mongoose.Schema;
 
 var userDataSchema = new Schema({
